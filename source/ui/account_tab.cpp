f1db1934b1a07940428ec84a03c57988650fa552
Riku32 <riku@kawaii.sh>
2022-04-10 00:30:18 -0500

    Fix i18n for lines

--- source/ui/account_tab.cpp
+++ source/ui/account_tab.cpp
@@ -6,8 +6,8 @@ using namespace i18n::literals;
 
 namespace ui {
     AccountTab::AccountTab() {
-        auto* button = new brls::ListItem("Choose Account");
-        button->registerAction("main/account/select_profile"_i18n, brls::Key::A, [] () {
+        auto* button = new brls::ListItem("main/account/select_profile"_i18n);
+        button->registerAction("brls/hints/select"_i18n, brls::Key::A, [] () {
             account::SwitchAccount::selectAccount();
             return true;
         });
